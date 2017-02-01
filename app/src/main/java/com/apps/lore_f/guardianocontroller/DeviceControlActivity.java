38ae77fbb0b2c46e0e42c1cc28bcb32cf1667ae4
lorenzofailla <lorenzofailla.developer@gmail.com>
2017-02-01 17:38:53 +0100

    da work

--- app/src/main/java/com/apps/lore_f/guardianocontroller/DeviceControlActivity.java
+++ app/src/main/java/com/apps/lore_f/guardianocontroller/DeviceControlActivity.java
@@ -110,8 +110,6 @@ public class DeviceControlActivity extends AppCompatActivity {
         });
 
         progressBar = (ProgressBar) findViewById(R.id.PBR___DEVICECONTROLACTIVITY___PROGRESSBAR);
-        linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setStackFromEnd(true);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -121,53 +119,6 @@ public class DeviceControlActivity extends AppCompatActivity {
         DatabaseReference listOfMedia = firebaseDatabaseReference.child(firebaseUser.getUid()).child(getString(R.string.GLOBAL___UPLOADED_MEDIA));
         listOfMedia.addValueEventListener(updateKeysArray);
 
-        firebaseAdapter = new FirebaseRecyclerAdapter<PictureTakenMessage, TakenPicturesHolder>(
-                PictureTakenMessage.class,
-                R.layout.taken_picture,
-                TakenPicturesHolder.class,
-                firebaseDatabaseReference.child(firebaseUser.getUid()).child(TAKEN_PICTURES_CHILD)) {
-
-            @Override
-            protected void populateViewHolder(TakenPicturesHolder viewHolder, final PictureTakenMessage pictureTakenMessage, final int position) {
-
-                progressBar.setVisibility(ProgressBar.INVISIBLE);
-
-                viewHolder.txvPictureDate.setText(pictureTakenMessage.getDateStamp());
-                viewHolder.btnGoToPicture.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        Intent intent = new Intent(getApplicationContext(), PictureViewerActivity.class);
-                        intent.putExtra("_media-id", mediaIDs[position]);
-                        startActivity(intent);
-
-                    }
-
-                });
-
-            }
-
-        };
-
-        firebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-                int onlineDevicesCount = firebaseAdapter.getItemCount();
-                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
-                // If the recycler view is initially being loaded or the user is at the bottom of the list, scroll
-                // to the bottom of the list to show the newly added message.
-                if (lastVisiblePosition == -1 ||
-                        (positionStart >= (onlineDevicesCount - 1) && lastVisiblePosition == (positionStart - 1))) {
-                    takenPicturesRecyclerView.scrollToPosition(positionStart);
-                }
-
-            }
-
-        });
-
-        takenPicturesRecyclerView.setLayoutManager(linearLayoutManager);
-        takenPicturesRecyclerView.setAdapter(firebaseAdapter);
 
     }
 
@@ -267,16 +218,74 @@ public class DeviceControlActivity extends AppCompatActivity {
 
             }
 
+            Log.d(TAG, "Finished array creation - " + mediaIDs.length+ " elements.");
+
+            refreshAdapter();
+
         }
 
         @Override
         public void onCancelled(DatabaseError databaseError) {
 
         }
+
     };
 
+    private void refreshAdapter(){
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+
+        firebaseAdapter = new FirebaseRecyclerAdapter<PictureTakenMessage, TakenPicturesHolder>(
+                PictureTakenMessage.class,
+                R.layout.taken_picture,
+                TakenPicturesHolder.class,
+                firebaseDatabaseReference.child(firebaseUser.getUid()).child(TAKEN_PICTURES_CHILD)) {
+
+            @Override
+            protected void populateViewHolder(TakenPicturesHolder viewHolder, final PictureTakenMessage pictureTakenMessage, final int position) {
+
+                progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+                viewHolder.txvPictureDate.setText(pictureTakenMessage.getDateStamp());
+                viewHolder.btnGoToPicture.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent intent = new Intent(getApplicationContext(), PictureViewerActivity.class);
+                        intent.putExtra("_media-id", mediaIDs[position]);
+                        startActivity(intent);
+
+                    }
+
+                });
+
+            }
+
+        };
+
+        firebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                int mediaCount = firebaseAdapter.getItemCount();
+                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+                // If the recycler view is initially being loaded or the user is at the bottom of the list, scroll
+                // to the bottom of the list to show the newly added message.
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (mediaCount - 1) && lastVisiblePosition == (positionStart - 1))) {
+                    takenPicturesRecyclerView.scrollToPosition(positionStart);
+                }
+
+            }
+
+        });
+
+        takenPicturesRecyclerView.setLayoutManager(linearLayoutManager);
+        takenPicturesRecyclerView.setAdapter(firebaseAdapter);
 
 
+    }
 
 
 
